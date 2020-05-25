657dd614a14cd4b2979de4e70afa673effa50e88
André Dourado <andrew.dourado.freitas@gmail.com>
2020-05-25 10:15:22 -0300

    Emails pelo Amazon SES

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'andre.dourado@hairmode.com.br',
+      name: 'André do Hairmode',
+    },
+  },
+} as IMailConfig;
